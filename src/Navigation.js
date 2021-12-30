1aa1b810e04adb2c35eb9f8d7a797d528c6ee9f7
Logan McLain <Sloggiebear@users.noreply.github.com>
2021-12-30 20:49:11 +0000

    Merging single people tax calculator features

---
+++ src/Navigation.js
@@ -0,0 +1,21 @@
+export default function navigation() {
+    return (
+    <nav className="nav">
+        <a className="back" href="https://amberandemerald.com/#projects">
+        <svg version="1.1" id="Capa_1" xmlns="http://www.w3.org/2000/svg" x="0px" y="0px"
+	 viewBox="0 0 195.055 195.055" >
+<g>
+	<g>
+		<polygon style={{fill: '#ffffff'}} points="33.138,94.882 73.787,54.245 70.047,50.501 23.025,97.527 70.047,144.55 73.787,140.813 
+			33.138,100.165 166.837,100.165 166.837,94.882 		"/>
+		<path style={{fill: '#ffffff'}} d="M97.529,0C43.753,0,0.005,43.751,0.005,97.531c0,53.769,43.748,97.524,97.524,97.524
+			s97.52-43.755,97.52-97.524C195.049,43.751,151.305,0,97.529,0z M97.529,190.441c-51.231,0-92.914-41.679-92.914-92.911
+			c0-51.242,41.683-92.925,92.914-92.925c51.228,0,92.914,41.683,92.914,92.921C190.443,148.759,148.757,190.441,97.529,190.441z"/>
+	</g>
+</g>
+</svg>
+Back to Projects</a>
+        <div><p>Calculator built with React with custom CSS and deployed on Firebase. Thanks to <a href="https://courses.webdevsimplified.com/" target="_blank" rel="noopener">Kyle</a> </p></div>
+    </nav>
+    )
+}
